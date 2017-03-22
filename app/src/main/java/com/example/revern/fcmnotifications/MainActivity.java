e4885ce63c2f744ed3701cdd3200c3cd37cc78a8
Almaz Iskhakov <almaz.iskhakov@flatstack.com>
2017-03-22 16:48:41 +0300

    add token displaying

--- app/src/main/java/com/example/revern/fcmnotifications/MainActivity.java
+++ app/src/main/java/com/example/revern/fcmnotifications/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.revern.fcmnotifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView uiToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uiToken = (TextView) findViewById(R.id.token);
+        uiToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uiToken.setText(FirebaseInstanceId.getInstance().getToken());
+            }
+        });
     }
 }
